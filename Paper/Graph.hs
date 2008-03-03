7c5a643a6bd4807f02dc929e38b0efc1a7c67297
Neil Mitchell <ndmitchell@gmail.com>
2008-03-03 23:46:47 +0000

    A load of work on the graph infrastructure

--- Paper/Graph.hs
+++ Paper/Graph.hs
@@ -1,10 +1,17 @@
 
 module Paper.Graph(graphLog) where
 
+import Control.Monad
 import Data.List
+import qualified Data.Map as Map
+import Data.Ord
 import System.Directory
 import System.FilePath
 import System.Time
+
+
+graphDir = "paper"
+graphFile = graphDir </> "graph.txt"
 
 
 graphLog :: [(FilePath,Int)] -> IO ()
@@ -16,6 +23,52 @@ graphLog xs = do
         f xs = do
             let dir = fst $ head xs
                 txt = map snd xs
-            createDirectoryIfMissing True (dir </> "paper")
-            appendFile (dir </> "paper" </> "graph.txt") (unlines txt)
+            createDirectoryIfMissing True graphDir
+            appendFile (dir </> graphFile) (unlines txt)
 
+
+-- load the data
+graphLoad :: [FilePath] -> IO [(TimeStep,FilePath,Int)]
+graphLoad xs =
+    liftM (sortBy (comparing fst3) . concat) $ mapM f $
+        group $ map splitFileName xs
+    where
+        f xs = do
+            let dir = fst (head xs)
+                files = map snd xs
+            src <- readFile $ dir </> graphFile
+            return [(timeStep a,b,c) | s <- lines src, not $ null s, let (a,b,c) = read s, b `elem` files]
+
+fst3 (x,_,_) = x
+
+type TimeStep = (Int,Int,Int)
+
+timeStep :: CalendarTime -> TimeStep
+timeStep t = (ctYear t, fromEnum (ctMonth t) + 1, ctDay t)
+
+
+incStep :: TimeStep -> TimeStep
+incStep (a,12,31) = (a+1,1,1)
+incStep (a, b,31) = (a,b+1,1)
+incStep (a, b, c) = (a,b,c+1)
+
+
+totalCounts :: [FilePath] -> [(TimeStep,FilePath,Int)] -> [(TimeStep,Int)]
+totalCounts _ [] = [((2000,1,1),0)]
+totalCounts files xs = f (Map.fromList (zip files $ repeat 0)) xs steps
+    where
+        steps = iterate incStep $ fst3 (head xs)
+
+        f now xxs@((a,b,c):xs) (s:ss)
+            | a > s = dump s now : f now xxs ss
+            | otherwise = f (Map.insert b c now) xs (s:ss)
+        f now [] (s:ss) = [dump s now]
+
+        dump s mp = (s, sum $ Map.elems mp)
+
+
+graphCreate :: [FilePath] -> IO ()
+graphCreate files = do
+    xs <- graphLoad files
+    xs <- return $ totalCounts files xs
+    return ()
