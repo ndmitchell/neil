3cbb37ef4855110cdc486c86696357aae4c04b22
Neil Mitchell <ndmitchell@gmail.com>
2008-03-05 16:55:22 +0000

    Modify LatexError to detect missing brackets

--- Paper/LatexError.hs
+++ Paper/LatexError.hs
@@ -3,7 +3,10 @@ module Paper.LatexError(latexError) where
 
 import Control.Monad
 import Data.Char
+import Data.List
+import Data.Maybe
 import System.Directory
+import System.FilePath
 
 
 -- given a list of all the files that went into the mix
@@ -11,7 +14,9 @@ import System.Directory
 -- and the errors reported to the screen
 -- attempt to diagnose something
 latexError :: [FilePath] -> FilePath -> String -> IO ()
-latexError texFiles logFile messages = mapM_ f $ lines messages
+latexError texFiles logFile messages = do
+        mapM_ f $ lines messages
+        mapM_ checkFile texFiles
     where
         rep from to x = if x == from then to else x
 
@@ -22,6 +27,7 @@ latexError texFiles logFile messages = mapM_ f $ lines messages
         g _ = return ()
 
 
+-- show a section of a file, centered around a particular point
 showSection :: FilePath -> Int -> IO ()
 showSection file pos = do
         b <- doesFileExist file
@@ -31,3 +37,34 @@ showSection file pos = do
     where f (p,s) = show p ++ " : " ++ s
 
 
+---------------------------------------------------------------------
+-- Check a file for obvious bracketing errors
+checkFile :: FilePath -> IO ()
+checkFile file = do
+        src <- readFile file
+        let errs = f [] (zip [0..] $ lines src)
+        putStr $ unlines ["Error in " ++ takeBaseName file ++ ":" ++ show n ++ ", " ++ x | (n,x) <- errs]
+    where
+        (open,shut) = unzip brackets
+        search x ys = listToMaybe [(i, drop (length y) x) | (i,y) <- zip [0..] ys, y `isPrefixOf` x]
+
+        f seen [] = if null seen then [] else
+                    [(n, "Bracket openned but never shut " ++ show (open !! i)) | (n,i) <- seen]
+        f seen ((n,[]):xs) = f seen xs
+        f seen ((n,x):xs) =
+                case (search x open, search x shut) of
+                    (Just (i,x),_) -> f ((n,i):seen) ((n,x):xs)
+                    (_,Just (i,x)) | null seen ->
+                        [(n,"Close bracket " ++ show (shut !! i) ++ ", but no open anywhere")]
+                                   | otherwise ->
+                        let ((n2,i2):seen2) = seen
+                        in if i2 == i then f seen2 ((n,x):xs)
+                           else [(n,"Bracket mismatch, " ++ show (open !! i2) ++ " openned on " ++ show n2 ++
+                                   " but shut with " ++ show (shut !! i))]
+                    _ -> f seen ((n,tail x):xs)
+
+
+brackets :: [(String,String)]
+brackets = let (*) = (,) in
+    ["{" * "}"
+    ]
