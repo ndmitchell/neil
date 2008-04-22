e00d676e5960b3ff1653c2bd43680e1974d2df56
Neil Mitchell <ndmitchell@gmail.com>
2008-04-22 14:38:04 +0000

    Add some string utilities

---
+++ Paper/Util/String.hs
@@ -0,0 +1,27 @@
+
+module Paper.Util.String where
+
+import Data.Char
+import Data.List
+
+
+dropUntilStr s xs | s `isPrefixOf` xs = drop (length s) xs
+dropUntilStr s (x:xs) = dropUntilStr s xs
+dropUntilStr s [] = []
+
+
+
+breakStr :: String -> String -> (String,String)
+breakStr s xs | s `isPrefixOf` xs = ("",xs)
+breakStr s (x:xs) = (x:a,b)
+    where (a,b) = breakStr s xs
+breakStr s [] = ([],[])
+
+
+
+splitStr s [] = []
+splitStr s xs = a : case splitStr s (drop (length s) b) of
+                         [] -> []
+                         x:xs -> (s++x):xs
+    where (a,b) = breakStr s xs
+
