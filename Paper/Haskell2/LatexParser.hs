ba4bd13a47d823d397465bee66922cdbabeb381e
Neil Mitchell <ndmitchell@gmail.com>
2008-05-10 15:14:18 +0000

    Add a low-level Latex parser

---
+++ Paper/Haskell2/LatexParser.hs
@@ -0,0 +1,58 @@
+
+module Paper.Haskell2.LatexParser(HsLow(..), latexParser) where
+
+import Data.Char
+import Data.List
+import Paper.Util.String
+
+
+data HsLow = HsDef String
+           | HsCheck Int Bool String String
+           deriving Show
+
+-- HsCheck a b c d
+--     a: line number 
+--     b: True is Expr, False is Stmt
+--     c: any proceeding command, possibly none
+--        \ignore, or \hs{command}
+--     d: code
+
+
+latexParser :: String -> [HsLow]
+latexParser = f 1 ""
+    where
+        f i cmd xs | "\\hsDef{" `isPrefixOf` xs = HsDef a : f (i + newlines a) "" b
+            where (a,b) = break (== '}') $ drop 7 xs
+
+        f i cmd xs | "\\ignore" `isPrefixOf` xs = f i "ignore" $ drop 7 xs
+
+        f i cmd xs | "\\hs{" `isPrefixOf` xs = f (i + newlines a) a $ drop 1 b
+            where (a,b) = break (== '}') $ drop 4 xs
+
+        f i cmd ('|':'|':xs) = f i "" xs
+        f i cmd ('|':xs) | '\n' `elem` a = error "Failed to parse | lines"
+                         | otherwise = HsCheck i True cmd a : f i "" b
+            where (a,b) = spanExpr xs
+
+        f i cmd xs | "\\begin{code}" `isPrefixOf` xs
+                   = HsCheck i False cmd a : f (i + newlines a) "" b
+            where (a,b) = breakStr "\\end{code}" $ drop 12 xs
+
+        f i cmd (x:xs) | x == '\n' = f (i+1) cmd xs
+                       | isSpace x = f i cmd xs
+                       | otherwise = f i "" xs
+        f i cmd [] = []
+
+
+spanExpr :: String -> (String, String)
+spanExpr ('|':'|':xs) = ('|':a, b)
+    where (a,b) = spanExpr xs
+spanExpr ('|':xs) = ("", xs)
+spanExpr (x:xs) = (x:a,b)
+    where (a,b) = spanExpr xs
+spanExpr [] = ("","")
+
+
+newlines :: String -> Int
+newlines = length . filter (== '\n')
+
