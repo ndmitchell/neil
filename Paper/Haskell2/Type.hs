3beb0c201fe1897da1f39cec40e5451a3c5eac3b
Neil Mitchell <ndmitchell@gmail.com>
2008-05-18 09:33:21 +0000

    Add @!not syntax for include items

--- Paper/Haskell2/Type.hs
+++ Paper/Haskell2/Type.hs
@@ -34,18 +34,26 @@ data HsType = Import
 
 data Where = Always
            | Only [String]
+           | OnlyNot [String]
              deriving Show
 
 
 parseWhere :: [String] -> Where
 parseWhere [] = Only ["default"]
-parseWhere xs = Only xs
+parseWhere xs | null neg = Only xs
+              | otherwise = OnlyNot $ map tail neg
+    where neg = filter ("!" `isPrefixOf`) xs
 
 
 allWhere :: [Where] -> [String]
-allWhere xs = nub $ sort $ concat [x | Only x <- xs]
+allWhere = nub . sort . concatMap f
+    where
+        f (Only x) = x
+        f (OnlyNot x) = x
+        f _ = []
 
 
 matchWhere :: String -> Where -> Bool
 matchWhere x Always = True
 matchWhere x (Only xs) = x `elem` xs
+matchWhere x (OnlyNot xs) = x `notElem` xs
