05dfae3c606677f54c2563e3dfff2d8d172e94eb
Neil Mitchell <ndmitchell@gmail.com>
2008-05-18 06:15:37 +0000

    Add errorDie to the error module

--- Paper/Util/Error.hs
+++ Paper/Util/Error.hs
@@ -1,11 +1,19 @@
 
-module Paper.Util.Error(errorMsg) where
+module Paper.Util.Error(errorMsg, errorDie) where
 
 import System.FilePath
 
 
 errorMsg :: FilePath -> Int -> String -> String -> IO ()
-errorMsg file line short long = putStrLn $
+errorMsg = errorWith putStrLn
+
+
+errorDie :: FilePath -> Int -> String -> String -> a
+errorDie = errorWith error
+
+
+errorWith :: (String -> a) -> FilePath -> Int -> String -> String -> a
+errorWith action file line short long = action $
     takeFileName file ++
     (if line == 0 then "" else "(" ++ show line ++ ")") ++
     " " ++ short ++
