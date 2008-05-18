4bac546650f89bf4058ab8be3b7d7e4bed716544
Neil Mitchell <ndmitchell@gmail.com>
2008-05-18 06:46:45 +0000

    Some basic processing in stage3

--- Paper/Haskell2/Stage4.hs
+++ Paper/Haskell2/Stage4.hs
@@ -1,9 +1,22 @@
 
 module Paper.Haskell2.Stage4(stage4) where
 
+import Data.List
 import Paper.Haskell2.Type
 
 
 stage4 :: [HsItem] -> String
-stage4 = show
+stage4 xs = unlines $ "module Temp where" : concatMap f xs
+    where
+        f (Stmt pos x _) = [linePragma pos, fixStmt x, ""]
+        f _ = [] -- TODO: Hides wrong answers!
 
+
+
+fixStmt = unlines . map f . lines
+    where
+        f x | "module" `isPrefixOf` x = "-- " ++ x
+            | otherwise = x
+
+        
+        
