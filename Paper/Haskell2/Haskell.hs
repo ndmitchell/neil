9f73eb947b631fa039fec8b2e65c661646de97d1
Neil Mitchell <ndmitchell@gmail.com>
2008-05-18 10:17:10 +0000

    Add fakeImplement function, which fills in stubs where necessary

--- Paper/Haskell2/Haskell.hs
+++ Paper/Haskell2/Haskell.hs
@@ -1,7 +1,8 @@
 
-module Paper.Haskell2.Haskell(defines, wants, rename) where
+module Paper.Haskell2.Haskell(defines, rename, fakeImplement) where
 
 import Data.Char
+import Data.List
 import Data.Maybe
 
 
@@ -9,18 +10,48 @@ defines :: String -> [String]
 defines = f . map lexer2 . lines
     where
         f ((x:"::":_):rest) = x : f rest
+        f (("(":x:")":"::":_):rest) = x : f rest
         f (_:rest) = f rest
         f [] = []
-
-
-wants :: String -> [String]
-wants _ = []
 
 
 rename :: [(String, String)] -> String -> String
 rename [] = id
 rename ren = concat . map f . lexer
     where f x = fromMaybe x $ lookup x ren
+
+
+-- figure out which definitions have a type signature
+-- but no associated body, and make one up
+fakeImplement :: String -> String
+fakeImplement xs = xs ++ "\n" ++ unlines (
+    [operator x ++ " = undefined -- stub" | x <- def \\ imp] ++
+    ["-- " ++ show def, "-- " ++ show imp])
+    where (def,imp) = (definesFunction xs, implementsFunction xs)
+
+
+-- more a "this function is definately defined"
+-- as conservative
+definesFunction :: String -> [String]
+definesFunction = concatMap (f . lexer2) . filter (not . isSpace . head) . filter (not .null) . lines
+    where
+        f ("(":x:")":"::":_) = [x]
+        f (x:"::":_) = [x]
+        f _ = []
+
+
+-- more a "this function is possibly implemented"
+-- as conservative
+implementsFunction :: String -> [String]
+implementsFunction = concatMap (f . lexer2) . lines
+    where
+        f (x:"::":xs) = []
+        f (x:xs) = [x]
+        f [] = []
+
+
+operator (x:xs) | isAlpha x = x:xs
+operator x = "(" ++ x ++ ")"
 
 
 -- concat . lexer == id
