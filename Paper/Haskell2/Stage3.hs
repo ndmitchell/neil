c36ce781714df874561100c78621c27c73faafcd
Neil Mitchell <ndmitchell@gmail.com>
2008-05-18 10:17:28 +0000

    Implement stubs in stage 3

--- Paper/Haskell2/Stage3.hs
+++ Paper/Haskell2/Stage3.hs
@@ -3,6 +3,7 @@ module Paper.Haskell2.Stage3(stage3) where
 
 import Data.List
 import Paper.Haskell2.Type
+import Paper.Haskell2.Haskell
 
 
 -- should be removing [T]Expr and [T]Variable
@@ -13,7 +14,7 @@ stage3 = map f
         f x = x
 
 
-fixStmt = unlines . map f . lines
+fixStmt = fakeImplement . unlines . map f . lines
     where
         f x | "module" `isPrefixOf` x = "-- " ++ x
             | otherwise = x
