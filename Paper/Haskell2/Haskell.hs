6fc35b885946746dcfecf1ba7ae27c4bc7c55257
Neil Mitchell <ndmitchell@gmail.com>
2008-05-19 12:02:09 +0000

    Add do as a keyword

--- Paper/Haskell2/Haskell.hs
+++ Paper/Haskell2/Haskell.hs
@@ -13,7 +13,7 @@ import Paper.Util.String
 isHaskellSymbol :: Char -> Bool
 isHaskellSymbol = flip elem "|+-*"
 
-haskellKeywords = ["class","instance","where","data","type","import","in","let"]
+haskellKeywords = ["class","instance","where","data","type","import","in","let","do"]
 
 defines :: String -> [String]
 defines = nub . filter validName . concatMap f . map lexer . classLeft . lines
