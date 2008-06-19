003dcda06d4bb8063b792d7d9fb48f9b636662a1
Neil Mitchell <ndmitchell@gmail.com>
2008-06-19 15:18:34 +0000

    Flesh out the ftp command a bit more

--- Paper/Ftp.hs
+++ Paper/Ftp.hs
@@ -1,8 +1,22 @@
 
 module Paper.Ftp(ftp) where
 
+import Control.Monad
+import System.Cmd
+import System.Directory
+import System.FilePath
 
 
 ftp :: FilePath -> IO ()
 ftp darcs = do
-    print "ftp: todo"
+    let name = takeFileName darcs
+        out = darcs </> name <.> "patch"
+    b <- doesFileExist out
+    when b $ removeFile out
+
+    system $ "darcs send" ++
+                 " --repodir=\"" ++ darcs ++ "\"" ++
+                 " --output=\"" ++ out ++ "\""
+    b <- doesFileExist out
+    when b $ do
+        print "push it"
