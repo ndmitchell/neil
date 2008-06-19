bed09204ccf4ea0fe118d2a3b1bc79ae24440a49
Neil Mitchell <ndmitchell@gmail.com>
2008-06-19 15:33:28 +0000

    Fully implement the ftp command

--- Paper/Ftp.hs
+++ Paper/Ftp.hs
@@ -5,12 +5,14 @@ import Control.Monad
 import System.Cmd
 import System.Directory
 import System.FilePath
+import System.IO
 
 
 ftp :: FilePath -> IO ()
 ftp darcs = do
     let name = takeFileName darcs
         out = darcs </> name <.> "patch"
+        ftpfile = out <.> "ftp"
     b <- doesFileExist out
     when b $ removeFile out
 
@@ -19,4 +21,30 @@ ftp darcs = do
                  " --output=\"" ++ out ++ "\""
     b <- doesFileExist out
     when b $ do
-        print "push it"
+        password <- getPassword
+        writeFile ftpfile $ unlines
+            ["ndm500"
+            ,password
+            ,"cd web/patches"
+            ,"binary"
+            ,"put " ++ (name <.> "patch")
+            ,"quit"]
+        system $ "ftp -s:" ++ ftpfile ++ " ftp.york.ac.uk"
+        removeFile ftpfile
+        removeFile out
+        return ()
+
+
+getPassword = do
+        e <- hGetEcho stdin
+        hSetEcho stdin False
+        putStr "Enter password: "
+        r <- f
+        hSetEcho stdin e
+        return r
+    where
+        f = do x <- getChar
+               if x == '\n' then return [] else do
+                   putStr "\b*"
+                   xs <- f
+                   return (x:xs)
