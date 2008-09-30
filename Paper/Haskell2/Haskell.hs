c1953b6eb4bdc3242a091cb56fbb55147a136217
Neil Mitchell <ndmitchell@gmail.com>
2008-09-30 15:43:30 +0000

    Add module and import as Haskell keywords

--- Paper/Haskell2/Haskell.hs
+++ Paper/Haskell2/Haskell.hs
@@ -13,7 +13,7 @@ import Paper.Util.String
 isHaskellSymbol :: Char -> Bool
 isHaskellSymbol = flip elem "|+-*<>"
 
-haskellKeywords = ["class","instance","where","data","type","import","in","let","do"]
+haskellKeywords = ["class","instance","where","data","type","import","in","let","do","module","import"]
 haskellKeySymbols = ["--"]
 
 defines :: String -> [String]
