b8c75db83a749d24144a7ec8a46656550334f803
Neil Mitchell <ndmitchell@gmail.com>
2010-10-06 11:26:44 +0000

    Switch to building with cabal

--- Main.hs
+++ Main.hs
@@ -1,21 +1,29 @@
 
 module Main(main) where
 
-import Control.Monad
-import Data.List
-import Data.Ord
 import System.Directory
 import System.Environment
 import System.FilePath
 
+import Paper.Util.FileData
 import Paper.Graph
+import Paper.Make
+import Paper.Talk
 import Paper.WordCount
+import Paper.Haskell
+import Paper.Haskell2.All
+import Paper.Push
+import Paper.Sync
+import Paper.Ftp
+import Paper.Ref
+import Paper.Todo
 
 
 {-
 COMMANDS:
 make -- compile the document
 wc -- word count
+wcmin -- minimal word count
 graph -- make a graph
 spell -- complete spell check
 colin -- auto-colin grammar check
@@ -27,6 +35,11 @@ all stored files go in paper/ directory
     the output from chart
     word count logs (used for charting)
     grammar checking logs
+
+all commands that take arguments take either:
+    nothing - use the current directory
+    a directory name - index.tex is the main file, all other *.tex files are extras
+    a list of files - first is the main file, all others are extras (all must be the same directory)
 -}
 
 main :: IO ()
@@ -34,52 +47,91 @@ main = do
     args <- getArgs
     case args of
         (cmd:files) -> do
-            files <- getFiles files
+            files <- getFileData files
             process cmd files
-        _ -> do process "" []
+        _ -> error "No arguments given"
 
 
-process :: String -> [FilePath] -> IO ()
-process "wc" files = do
-    let shw = fixed ("total" : map takeBaseName files)
-    res <- flip mapM files $ \file -> do
-        putStr $ shw (takeBaseName file) ++ "  "
-        count <- wordCount file
+wc :: (FilePath -> IO Int) -> FileData -> IO ()
+wc f files = do
+    let shw = fixed ("total" : map dropExtension (allFiles files))
+    res <- flip mapM (allFiles files) $ \file -> do
+        putStr $ shw (dropExtension file) ++ "  "
+        count <- f (directory files </> file)
         putStrLn $ int count
         return (file,count)
     putStrLn $ shw "Total" ++ "  " ++ int (sum $ map snd res)
-    graphLog res
+    root <- settingsDir files
+    graphLog (root </> "graph.txt") res
+
+process :: String -> FileData -> IO ()
+process "wc" files = wc wordCountNorm files
+process "wcmin" files = wc wordCountMin files
 
 process "graph" files = do
-    let res = dropFileName (getMainFile files) </> "paper" </> "graph.png"
-    graphCreate files res
+    root <- settingsDir files
+    let res = root </> "graph.png"
+    graphCreate (root </> "graph.txt") res (allFiles files)
+    putStrLn $ "Written graph, " ++ res
+
+process "make" files = do
+    dat <- dataDir
+    obj <- objDir "make" files
+    make dat obj (directory files) (mainFile files) (allFiles files)
+
+process "haskell" files = do
+    obj <- objDir "haskell" files
+    haskell obj files
+
+process "haskell2" files = do
+    obj <- objDir "haskell2" files
+    haskell2 obj (argFiles files)
+
+process "talk" files = do
+    tlk <- objDir "talk" files
+    talk tlk (argFiles files) (flags files)
+
+process "push" files = push (darcs files)
+
+process "sync" files = do
+    dat <- dataDir
+    sync (takeDirectory $ takeDirectory dat)
+
+process "ref" files = ref (argFiles files)
+
+process "todo" files = todo (argFiles files)
+
+process "ftp" files = ftp (darcs files)
+
+process "check" files = do
+    mapM_ (`process` files) ["ref","todo"]
+    putStrLn "All checks succeeded"
 
 process x files = putStrLn $ "Error: Unknown action, " ++ show x
 
 
+----- utility stuff
+
+-- the directory where data files (paper.bib/paper.fmt) live
+dataDir :: IO FilePath
+dataDir = do
+    x <- findExecutable "paper"
+    case x of
+        Nothing -> error "Couldn't find the data directory"
+        Just y -> return $ dropFileName y </> "data"
+
+settingsDir :: FileData -> IO FilePath
+settingsDir = ensureDir "paper"
+
+objDir :: String -> FileData -> IO String
+objDir cmd = ensureDir ("obj" </> cmd)
 
 
-getFiles :: [String] -> IO [FilePath]
-getFiles [] = do
-    d <- getCurrentDirectory
-    getFiles [d]
-getFiles x = liftM concat $ mapM (\x -> canonicalizePath x >>= getFile) x
-
-
-getFile :: String -> IO [FilePath]
-getFile x = do
-    b <- doesDirectoryExist x
-    if b then do
-        s <- getDirectoryContents x
-        return $ filter ((==) ".tex" . takeExtension) $ map (x </>) s
-     else do
-        b <- doesFileExist x
-        if b then return [x] else error $ "File not found: " ++ x
-
-
-getMainFile :: [FilePath] -> FilePath
-getMainFile xs = snd $ maximumBy (comparing fst) [(rank x, x) | x <- xs]
-    where rank x = liftM negate $ findIndex (== takeBaseName x) (reverse $ splitDirectories x)
+ensureDir :: FilePath -> FileData -> IO FilePath
+ensureDir name files = do
+    let s = directory files </> name
+    createDirectoryIfMissing True s
+    return s
 
 
 ----- mini formatting library
