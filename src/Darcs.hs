e9aaebf8531b2d82d48a9ede9833a45ae0b815c4
Neil Mitchell <ndmitchell@gmail.com>
2010-10-06 17:13:56 +0000

    Start transitioning to the new utility tool

---
+++ src/Darcs.hs
@@ -0,0 +1,137 @@
+
+module Darcs(push,pull,send,whatsnew,apply) where
+
+import Control.Monad
+import Data.Char
+import Data.List
+import System.Directory
+import System.Exit
+import System.FilePath
+import Util
+import Control.Exception as E
+import Control.Concurrent
+import Data.Maybe
+
+
+---------------------------------------------------------------------
+-- UTILITIES
+
+-- Find all the repos. Look down searching for a _darcs, if you find any, return them.
+-- If you don't find any, look up.
+findRepos :: FilePath -> IO [FilePath]
+findRepos x = do
+    putStr "Looking for repos... "
+    x <- canonicalizePath x
+    xs <- searchDown x
+    res <- if null xs then searchUp x else return xs
+    putStrLn $ show (length res) ++ " found"
+    return res
+    where
+        searchDown dir = do
+            b <- doesDirectoryExist $ dir </> "_darcs"
+            if b then return [dir] else do
+                xs <- getDirectoryContents dir
+                xs <- return [dir </> x | x <- xs, not $ all (== '.') x]
+                fmap concat $ forM xs $ \x -> do
+                    b <- doesDirectoryExist x
+                    if b then searchDown x else return []
+
+        searchUp x = return []
+
+
+-- Run over each repo
+forEachRepo :: Bool -> FilePath -> (FilePath -> IO (Maybe String)) -> IO ()
+forEachRepo deleteLocks repo act = do
+    res <- fmap sort $ findRepos repo
+    when (null res) $ do
+        putStrLn $ "Error: No repos found relative to " ++ show repo
+        exitFailure
+
+    ln <- liner
+    finished <- newEmptyMVar
+    todo <- newMVar res
+    let done x msg = modifyMVar_ todo $ \old -> do
+        let new = delete x old
+        when (isJust msg) $ ln $ takeFileName x ++ ": " ++ fromJust msg ++ "\n"
+        if null new then do
+            ln "Finished\n"
+            putMVar finished ()
+         else
+            ln $ "Waiting for " ++ show (length new) ++ " (" ++ unwords (map takeFileName $ take 4 new) ++ "...)"
+        return new
+
+    done "" Nothing
+    forM_ res $ \repo -> forkIO $ do
+        let lockFile = repo </> "_darcs/lock"
+        b <- doesFileExist lockFile
+        ans <- if b && not deleteLocks then
+            return $ Just "skipping because it has a lock outstanding"
+         else do
+            when (b && deleteLocks) $ removeFile lockFile
+            act repo
+        done repo ans
+
+    takeMVar finished
+
+---------------------------------------------------------------------
+-- COMMANDS
+
+whatsnew :: FilePath -> Bool -> IO ()
+whatsnew repo locks = forEachRepo locks repo $ \x ->
+    (do
+        changes <- f ("darcs whatsnew --summary --repo=" ++ x) length
+        local <- f ("darcs send --dry-run --all --repo=" ++ x) $ hasnt "No recorded local changes to send!"
+        remote <- f ("darcs pull --dry-run --all --repo=" ++ x) $ spaces 2
+        return $ if changes == 0 && local == 0 && remote == 0 then Nothing else
+            Just $ show (changes,local,remote) ++ " changes"
+    ) `E.onException` (do
+        let lockFile = x </> "_darcs/lock"
+        b <- doesFileExist lockFile
+        when b $ removeFile lockFile
+    )
+    where
+        hasnt msg out = if msg `elem` out then 0 else 1
+        spaces n out = length [() | x:xs <- out, not $ isSpace x] - n
+
+        f cmd count = do
+            (code,out,err) <- cmdCodeOutErr cmd
+            case code of
+                ExitFailure 1 -> return 0
+                ExitSuccess -> return $ count $ lines out
+                _ -> return $ negate 1
+
+
+
+{-
+    sync :: FilePath -> IO ()
+    sync dir = do
+        b <- doesDirectoryExist $ dir </> "_darcs"
+        if b then check dir else do
+            xs <- getDirectoryContents dir
+            xs <- return [dir </> x | x <- xs, not $ all (== '.') x]
+            flip mapM_ xs $ \x -> do
+                b <- doesDirectoryExist x
+                when b $ sync x
+    
+    
+    check :: FilePath -> IO ()
+    check dir = do
+        b1 <- run "darcs whatsnew --summary" "No changes!"
+        b2 <- run "darcs send --dry-run --all" "No recorded local changes to send!"
+        let msg = ["changes"|b1] ++ ["patches"|b2]
+        when (msg /= []) $
+            putStrLn $ unwords $ [takeFileName dir,"has"] ++ intersperse "and" msg
+        where
+            run cmd want = do
+                let out = dir </> "paper_sync.tmp"
+                system $ cmd ++ " --repodir=\"" ++ dir ++ "\" > \"" ++ out ++ "\""
+                src <- readFile' out
+                return $ want `notElem` lines src
+-}
+
+
+
+push = undefined
+pull = undefined
+send = undefined
+apply = undefined
---
+++ src/Util.hs
@@ -0,0 +1,73 @@
+
+module Util where
+
+import Control.Monad
+import System.Directory
+import System.Exit
+import System.FilePath
+import System.Directory
+import Control.Exception as E
+import System.IO
+import System.Cmd
+import Data.List
+import Control.Concurrent
+
+
+withTempFile :: (FilePath -> IO a) -> IO a
+withTempFile f = E.bracket
+    (do (file,h) <- openTempFile "." "neil.tmp"; hClose h; return file)
+    removeFile
+    f
+
+
+cmdCodeOutErr :: String -> IO (ExitCode, String, String)
+cmdCodeOutErr x = withTempFile $ \stderr -> withTempFile $ \stdout -> do
+    res <- system $ x ++ " > " ++ stdout ++ " 2> " ++ stderr
+    err <- readFile' stderr
+    out <- readFile' stdout
+    return (res,out,err)
+
+{-
+
+cmd :: String -> IO ()
+cmd x = do
+    res <- system x
+    when (res /= ExitSuccess) $
+        error $ "Failed in system command: " ++ x
+
+cmdOut :: String -> IO String
+cmdOut x = withTempFile $ \file -> do
+    cmd $ x ++ " >> " ++ file
+    readFile' file
+
+cmdPassOut :: String -> IO (Bool,String)
+cmdPassOut x = withTempFile $ \file -> do
+    b <- cmdPass $ x ++ " >> " ++ file
+    fmap ((,) b) $ readFile' file
+
+cmdPass :: String -> IO Bool
+cmdPass x = do
+    res <- system x
+    putStrLn $ x ++ " => " ++ show res
+    return $ res == ExitSuccess
+-}
+
+
+
+readFile' :: FilePath -> IO String
+readFile' file = do
+    h <- openFile file ReadMode
+    src <- hGetContents h
+    length src `seq` hClose h
+    return src
+
+
+liner :: IO (String -> IO ())
+liner = do
+    mvar <- newMVar 0
+    return $ \msg -> modifyMVar_ mvar $ \old -> do
+        let eol = "\n" `isSuffixOf` msg
+        msg <- return $ takeWhile (/= '\n') msg
+        let new = length msg
+        putStr $ (if old == 0 then "" else replicate 1000 '\b') ++ msg ++ replicate (old - new) ' '
+        if eol then putStrLn "" >> return 0 else return new
