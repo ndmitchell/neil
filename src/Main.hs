8cf2e200337a6d4db45d54de9c57dfc3301bc356
Neil Mitchell <ndmitchell@gmail.com>
2013-10-11 18:30:27 +0100

    Integrate back in the Paper commands as a custom command

--- src/Main.hs
+++ src/Main.hs
@@ -4,17 +4,22 @@ module Main(main) where
 import Control.Monad
 import Data.Maybe
 import System.Console.CmdArgs
+import System.Environment
 import System.IO
 
 import Arguments
 import Cabal
 import Darcs
+import qualified Paper.Main as Paper
 
 
 main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering
-    args <- cmdArgsRun arguments
-    fromMaybe (error $ "Don't know how to deal with argument: " ++ show args) $
-        msum [Darcs.run args, Cabal.run args]
-           
+    a <- getArgs
+    case a of
+        "paper":a -> withArgs a Paper.main
+        _ -> do
+            args <- cmdArgsRun arguments
+            fromMaybe (error $ "Don't know how to deal with argument: " ++ show args) $
+                msum [Darcs.run args, Cabal.run args]
