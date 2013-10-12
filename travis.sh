3adbb6716161e48e5ca4361e3a1f47eb1bf99d1a
Neil Mitchell <ndmitchell@gmail.com>
2013-10-12 22:03:31 +0100

    Remove an echo from the travis script

--- travis.sh
+++ travis.sh
@@ -3,7 +3,6 @@
 # It bootstraps to grab the 'neil' tool and run 'neil test'
 set -e # exit on errors
 set -x # echo each line
-echo Using the 'neil test' bootstrap script
 git clone https://github.com/ndmitchell/neil
 (cd neil && cabal install)
 neil test
