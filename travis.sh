e620b3a19139358f638742fae17c7808575e559b
Neil Mitchell <ndmitchell@gmail.com>
2013-10-12 10:28:09 +0100

    Add a note in the travis.sh script that I am bootstrapping

--- travis.sh
+++ travis.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 # This script is invoked from my Travis-CI commands
 # It bootstraps to grab the 'neil' tool and run 'neil test'
+echo Using the 'neil test' bootstrap script
 git clone https://github.com/ndmitchell/neil
 (cd neil && cabal install)
 neil test
