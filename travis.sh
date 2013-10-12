310498c964be4a16350a2a173dfbcee0768c460e
Neil Mitchell <ndmitchell@gmail.com>
2013-10-12 20:03:56 +0100

    Set -x and -e in the testing script

--- travis.sh
+++ travis.sh
@@ -1,6 +1,8 @@
-#!/bin/bash -e
+#!/bin/bash
 # This script is invoked from my Travis-CI commands
 # It bootstraps to grab the 'neil' tool and run 'neil test'
+set -e # exit on errors
+set -x # echo each line
 echo Using the 'neil test' bootstrap script
 git clone https://github.com/ndmitchell/neil
 (cd neil && cabal install)
