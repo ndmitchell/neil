e792e9dcef3cf609976740bc322e67f46889c9a3
Neil Mitchell <ndmitchell@gmail.com>
2013-10-12 10:12:23 +0100

    Comments in travis.sh

--- travis.sh
+++ travis.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
+# This script is invoked from my Travis-CI commands
+# It bootstraps to grab the 'neil' tool and run 'neil test'
 cabal configure --enable-tests
 cabal build
 cabal test
