4f5658210060a80af25cbf3c3318d93096c4345d
Neil Mitchell <ndmitchell@gmail.com>
2013-10-14 08:20:06 +0100

    Make the SDist tarball trick also performed by the test command

--- src/Cabal.hs
+++ src/Cabal.hs
@@ -8,18 +8,17 @@ import Data.List
 import Data.Maybe
 import Data.Functor
 import System.Directory
-import System.Exit
 import System.FilePath
 import Util
 import Arguments
 
-
----------------------------------------------------------------------
--- COMMANDS
-
 -- Policy: currently all must build flawlessly on 6.12.3, 7.0.2 and 7.2.1, and at least build on 6.10.4
 defOfficial = ["6.12.3","7.0.4","7.2.2","7.4.1","7.6.1"]
 defPartial = ["6.10.4"]
+
+
+---------------------------------------------------------------------
+-- COMMANDS
 
 
 -- | Check the .cabal file is well formed
@@ -28,54 +27,59 @@ cabalCheck = do
     res <- cmdCode "cabal check"
     checkCabalFile
 
+-- | Run some commands in a temporary directory with the unpacked cabal
+withSDist :: IO a -> IO a
+withSDist run = withTempDirectory $ \tdir -> do
+    cmd $ "cabal configure --builddir=" ++ tdir
+    cmd $ "cabal sdist --builddir=" ++ tdir
+    files <- getDirectoryContents tdir
+    let tarball = head $ [x | x <- files, ".tar.gz" `isSuffixOf` x]
+    withDirectory tdir $ cmd $ "tar -xf " ++ tarball
+    withDirectory (tdir </> dropExtension (dropExtension $ takeFileName tarball)) run
+
 
 run :: Arguments -> Maybe (IO ())
 run Test = Just $ do
-    putStrLn "neil test: starting test"
     cabalCheck
-    cmd "cabal configure --enable-tests"
-    cmd "cabal build"
-    cmd "cabal test"
-    putStrLn "neil test: finishing test"
+    withSDist $ do
+        cmd "cabal install --only-dependencies"
+        cmd $ "cabal configure --enable-tests --disable-library-profiling " ++
+              {- "--ghc-option=-Werror -} "--ghc-option=-fno-warn-warnings-deprecations" -- CABAL BUG WORKAROUND :(
+        cmd "cabal build"
+        cmd "cabal test"
 
 run Check = Just cabalCheck
 
 run Sdist{..} = Just $ do
+    cabalCheck
     tested <- testedWith
     tested <- return $ if null tested then [""] else tested
-    withTempDirectory $ \tdir -> do
-        cabalCheck
-        cmd $ "cabal configure --builddir=" ++ tdir
-        cmd $ "cabal sdist --builddir=" ++ tdir
-        files <- getDirectoryContents tdir
-        let tarball = head $ [x | x <- files, ".tar.gz" `isSuffixOf` x]
-        withDirectory tdir $ cmd $ "tar -xf " ++ tarball
-        withDirectory (tdir </> dropExtension (dropExtension $ takeFileName tarball)) $ do
-            let a +| b = if null a then b else a
-            forM_ (official +| defOfficial) $ \x -> do
+    withSDist $ do
+        let a +| b = if null a then b else a
+        forM_ (official +| defOfficial) $ \x -> do
+            putStrLn $ "Building with " ++ x
+            cmd "cabal clean"
+            cmd $ "cabal install --only-dependencies " ++
+                  "--with-compiler=c:\\ghc\\ghc-" ++ x ++ "\\bin\\ghc.exe --with-haddock=c:\\ghc\\ghc-" ++ x ++ "\\bin\\haddock.exe " ++
+                  "--with-hc-pkg=c:\\ghc\\ghc-" ++ x ++ "\\bin\\ghc-pkg.exe " ++
+                  "--flags=testprog"
+            cmd $ "cabal configure --ghc-option=-fwarn-unused-imports --disable-library-profiling " ++
+                  (if ignore_warnings then "" else "--ghc-option=-Werror ") ++
+                  "--ghc-option=-fno-warn-warnings-deprecations " ++ -- CABAL BUG WORKAROUND :(
+                  "--with-compiler=c:\\ghc\\ghc-" ++ x ++ "\\bin\\ghc.exe --with-haddock=c:\\ghc\\ghc-" ++ x ++ "\\bin\\haddock.exe " ++
+                  "--with-hc-pkg=c:\\ghc\\ghc-" ++ x ++ "\\bin\\ghc-pkg.exe " ++
+                  "--flags=testprog"
+            cmd "cabal build"
+            cmd "cabal haddock --executables"
+        unless ignore_partial $ do
+            forM_ (partial +| defPartial) $ \x -> do
                 putStrLn $ "Building with " ++ x
                 cmd "cabal clean"
                 cmd $ "cabal install --only-dependencies " ++
                       "--with-compiler=c:\\ghc\\ghc-" ++ x ++ "\\bin\\ghc.exe --with-haddock=c:\\ghc\\ghc-" ++ x ++ "\\bin\\haddock.exe " ++
-                      "--with-hc-pkg=c:\\ghc\\ghc-" ++ x ++ "\\bin\\ghc-pkg.exe " ++
-                      "--flags=testprog"
-                cmd $ "cabal configure --ghc-option=-fwarn-unused-imports --disable-library-profiling " ++
-                      (if ignore_warnings then "" else "--ghc-option=-Werror ") ++
-                      "--ghc-option=-fno-warn-warnings-deprecations " ++ -- CABAL BUG WORKAROUND :(
-                      "--with-compiler=c:\\ghc\\ghc-" ++ x ++ "\\bin\\ghc.exe --with-haddock=c:\\ghc\\ghc-" ++ x ++ "\\bin\\haddock.exe " ++
-                      "--with-hc-pkg=c:\\ghc\\ghc-" ++ x ++ "\\bin\\ghc-pkg.exe " ++
-                      "--flags=testprog"
+                      "--with-hc-pkg=c:\\ghc\\ghc-" ++ x ++ "\\bin\\ghc-pkg.exe"
+                cmd $ "cabal configure --disable-library-profiling --with-compiler=c:\\ghc\\ghc-" ++ x ++ "\\bin\\ghc.exe --with-hc-pkg=c:\\ghc\\ghc-" ++ x ++ "\\bin\\ghc-pkg.exe --flags=testprog"
                 cmd "cabal build"
-                cmd "cabal haddock --executables"
-            unless ignore_partial $ do
-                forM_ (partial +| defPartial) $ \x -> do
-                    putStrLn $ "Building with " ++ x
-                    cmd "cabal clean"
-                    cmd $ "cabal install --only-dependencies " ++
-                          "--with-compiler=c:\\ghc\\ghc-" ++ x ++ "\\bin\\ghc.exe --with-haddock=c:\\ghc\\ghc-" ++ x ++ "\\bin\\haddock.exe " ++
-                          "--with-hc-pkg=c:\\ghc\\ghc-" ++ x ++ "\\bin\\ghc-pkg.exe"
-                    cmd $ "cabal configure --disable-library-profiling --with-compiler=c:\\ghc\\ghc-" ++ x ++ "\\bin\\ghc.exe --with-hc-pkg=c:\\ghc\\ghc-" ++ x ++ "\\bin\\ghc-pkg.exe --flags=testprog"
-                    cmd "cabal build"
     cmd "cabal sdist"
     putStrLn $ "Ready to release!"
 
