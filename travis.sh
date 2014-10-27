fa4926b687e0c2bc0f16a52cd150b983930edcb7
Neil Mitchell <ndmitchell@gmail.com>
2014-10-27 21:53:03 +0000

    Add lots of retries

--- travis.sh
+++ travis.sh
@@ -2,11 +2,15 @@
 # This script is invoked from my Travis-CI commands
 # It bootstraps to grab the 'neil' tool and run 'neil test'
 set -e # exit on errors
+set -u # warn about unitialised variables
 set -x # echo each line
-cabal update
-cabal install --only-dependencies --enable-tests
-git clone https://github.com/ndmitchell/neil
-(cd neil && cabal install --flags=small)
+
+function retry(){ "$@" || "$@" || "$@"; }
+
+retry cabal update
+retry cabal install --only-dependencies --enable-tests
+retry git clone https://github.com/ndmitchell/neil
+(cd neil && retry cabal install --flags=small)
 export PATH=/home/travis/.cabal/bin:$PATH
 neil test --install
 if [ -e travis.hs ]; then
