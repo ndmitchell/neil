e16e388851bdb1485f447ae827d846f4fcca73db
Neil Mitchell <ndmitchell@gmail.com>
2014-10-28 19:10:19 +0000

    Get the cabal path right

--- travis.sh
+++ travis.sh
@@ -10,7 +10,7 @@ if [ "$GHCVER" != "" ]; then
     retry sudo add-apt-repository -y ppa:hvr/ghc
     retry sudo apt-get update
     retry sudo apt-get install ghc-$GHCVER cabal-install-1.20 happy-1.19.4 alex-3.1.3
-    export PATH=/opt/ghc/$GHCVER/bin:/opt/cabal/1.18/bin:/opt/happy/1.19.4/bin:/opt/alex/3.1.3/bin:/home/travis/.cabal/bin:$PATH
+    export PATH=/opt/ghc/$GHCVER/bin:/opt/cabal/1.20/bin:/opt/happy/1.19.4/bin:/opt/alex/3.1.3/bin:/home/travis/.cabal/bin:$PATH
 fi
 
 retry cabal update
