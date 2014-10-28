1c06586594b26ea953e3a73495937c883fdacd8b
Neil Mitchell <ndmitchell@gmail.com>
2014-10-28 13:07:47 +0000

    Move more logic over to the travis.sh script

--- travis.sh
+++ travis.sh
@@ -7,11 +7,17 @@ set -x # echo each line
 
 retry(){ "$@" || "$@" || "$@"; }
 
+if [ "$GHCVER" -ne "" ]; then
+    retry sudo add-apt-repository -y ppa:hvr/ghc
+    retry sudo apt-get update
+    retry sudo apt-get install cabal-install-1.18 ghc-$GHCVER
+    export PATH=/opt/ghc/$GHCVER/bin:/opt/cabal/1.18/bin:/home/travis/.cabal/bin:$PATH
+fi
+
 retry cabal update
 retry cabal install --only-dependencies --enable-tests
 retry git clone https://github.com/ndmitchell/neil
 (cd neil && retry cabal install --flags=small)
-export PATH=/home/travis/.cabal/bin:$PATH
 neil test --install
 if [ -e travis.hs ]; then
     runhaskell travis.hs
