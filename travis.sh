6c8ec2e296a9ab7964abf15cec03378fcfb6eab9
Neil Mitchell <ndmitchell@gmail.com>
2014-11-01 21:20:20 +0000

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -11,7 +11,6 @@ if [ "$GHCVER" != "" ]; then
     retry sudo apt-get update
     retry sudo apt-get install ghc-$GHCVER cabal-install-1.18 happy-1.19.4 alex-3.1.3
     export PATH=/opt/ghc/$GHCVER/bin:/opt/cabal/1.18/bin:/opt/happy/1.19.4/bin:/opt/alex/3.1.3/bin:/home/travis/.cabal/bin:$PATH
-    echo exposing binary here
     sudo /opt/ghc/$GHCVER/bin/ghc-pkg expose binary # on GHC 7.2 it is installed, but not exposed
 fi
 
