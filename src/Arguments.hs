6c4ddd72724970ecc43ea9938e8ae59463433537
Neil Mitchell <ndmitchell@gmail.com>
2014-11-23 21:19:23 +0000

    Remove the --delete-locks argument everywhere

--- src/Arguments.hs
+++ src/Arguments.hs
@@ -7,8 +7,8 @@ import System.Console.CmdArgs
 
 data Arguments
     -- darcs stuff
-    = Whatsnew {repo :: FilePath, delete_locks :: Bool, local :: Bool, look_for_adds :: Bool, ssh :: Bool}
-    | Pull {repo :: FilePath, delete_locks :: Bool}
+    = Whatsnew {repo :: FilePath, local :: Bool, look_for_adds :: Bool, ssh :: Bool}
+    | Pull {repo :: FilePath}
     | Push {repo :: FilePath, ssh :: Bool}
     | Send {repo :: FilePath, patch :: FilePath}
     | Apply {patch :: FilePath}
@@ -25,7 +25,6 @@ data Arguments
 
 arguments = cmdArgsMode $ modes
     [Whatsnew {repo = "." &= typDir &= help "Repo to use"
-              ,delete_locks = False &= help "Delete lock files"
               ,local = False &= help "Only check for local changes, no network required"
               ,look_for_adds = False &= name "l" &= help "Look for files to add"
               ,ssh = False &= help "SSH connections only, for if the http is down"}
