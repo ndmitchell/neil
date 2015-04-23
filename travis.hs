b2f9129f776c4776f2caaa09c46b50562ff584cc
Neil Mitchell <ndmitchell@gmail.com>
2015-04-23 21:15:29 +0100

    Add a travis.hs file to the test

---
+++ travis.hs
@@ -0,0 +1,10 @@
+-- If a travis.hs file is present in the repo, it will be compiled and executed
+-- after doing the 'cabal test'.
+
+import System.Time.Extra
+import System.Process.Extra
+
+main :: IO ()
+main = do
+    (time,_) <- duration $ system_ $ "neil +RTS -T -M100M -RTS --help"
+    putStrLn $ "Running --help took " ++ showDuration time
