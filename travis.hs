23a37a824265d994cf4d907e4f8ac3f1f1b071ee
Neil Mitchell <ndmitchell@gmail.com>
2016-02-08 09:28:49 +0000

    Add a test for GHC 8.0 regressions

--- travis.hs
+++ travis.hs
@@ -3,8 +3,10 @@
 
 import System.Time.Extra
 import System.Process.Extra
+import System.Directory
 
 main :: IO ()
 main = do
+    print =<< getCurrentDirectory
     (time,_) <- duration $ system_ $ "neil +RTS -M100M -RTS --help"
     putStrLn $ "Running --help took " ++ showDuration time
