a40d1229c39dd210f0a42cdb3b18ba83ffc12a7d
Neil Mitchell <ndmitchell@gmail.com>
2017-05-10 20:35:07 +0100

    Significant upgrade to the Travis binary downloader

--- misc/travis.sh
+++ misc/travis.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
-VERSION=2.0.6
-TEMP=$(mktemp --directory .hlint-XXXXX)
-mkdir $TEMP
-curl -o$TEMP/hlint.tar.gz -L --insecure https://github.com/ndmitchell/hlint/releases/download/v$VERSION/hlint-$VERSION-x86_64-linux.tar.gz
-tar -xzf $TEMP/hlint.tar.gz -C$TEMP
-$TEMP/hlint-$VERSION/hlint $*
+# This script is invoked from my Travis commands
+# It bootstraps to grab the a binary release and run it
+set -e # exit on errors
+
+PACKAGE=$1
+shift
+
+if [ -z "$PACKAGE" ]; then
+    echo No arguments provided, please pass the project name as the first argument
+    exit 1
+fi
+
+echo Downloading and running $PACKAGE...
+RELEASES=$(curl --silent https://api.github.com/repos/ndmitchell/$PACKAGE/releases)
+URL=$(echo $RELEASES | sed -e 's/.*"browser_download_url" *: *"\([^"]*-x86_64-linux\.tar\.gz\)".*/\1/')
+VERSION=$(echo $URL | sed -e 's/.*-\([\.0-9]\+\)-x86_64-linux\.tar\.gz/\1/')
+TEMP=$(mktemp --directory .$PACKAGE-XXXXX)
+
+cleanup(){
+    rm -r $TEMP
+}
+trap cleanup EXIT
+
+curl --progress-bar --location -o$TEMP/$PACKAGE.tar.gz $URL
+tar -xzf $TEMP/$PACKAGE.tar.gz -C$TEMP
+$TEMP/$PACKAGE-$VERSION/$PACKAGE $*
