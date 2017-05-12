9fc35d49578279fe905319b8da28dbed99a86440
Neil Mitchell <ndmitchell@gmail.com>
2017-05-12 17:16:46 +0100

    Update the appveyor script

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,16 +1,21 @@
 # This script is invoked from my Appveyor commands
 # It bootstraps to install stack and run the tests
 $ErrorActionPreference = "Stop"
-Set-PSDebug -Trace 2
+Set-PSDebug -Trace 1
 
 Invoke-WebRequest 'http://www.stackage.org/stack/windows-i386' -OutFile 'stack.zip'
 7z x stack.zip stack.exe
 
-Invoke-Expression (Invoke-WebRequest 'https://raw.githubusercontent.com/ndmitchell/hlint/master/misc/appveyor.ps1')
+$HLINT_ARGUMENTS=$env:HLINT_ARGUMENTS
+if ($HLINT_ARGUMENTS -eq '') {
+    $HLINT_ARGUMENTS = '.'
+}
+$Script = Invoke-WebRequest 'https://raw.githubusercontent.com/ndmitchell/hlint/master/misc/appveyor.ps1'
+Invoke-Command ([Scriptblock]::Create($Script.Content)) -ArgumentList $HLINT_ARGUMENTS
 
 Set-Variable STACK_ROOT 'c:\\sr'
 stack init
-stack setup > nul
+stack setup | Out-Null
 Write-Output "" | stack --no-terminal build --test --bench
 if ($LASTEXITCODE -ne 0){
     exit 1
