299602d42ddc94241817510c267369f71737f9f4
Neil Mitchell <ndmitchell@gmail.com>
2017-08-09 22:21:24 +0100

    Fix powershell environment munging

--- appveyor.ps1
+++ appveyor.ps1
@@ -2,15 +2,15 @@
 # It bootstraps to install stack and run the tests
 $ErrorActionPreference = "Stop"
 
-$HLINT_ARGUMENTS=$env:HLINT_ARGUMENTS
+$HLINT_ARGUMENTS = $env:HLINT_ARGUMENTS
 if ("$HLINT_ARGUMENTS" -eq '') {
     $HLINT_ARGUMENTS = '.'
 }
 $Script = Invoke-WebRequest 'https://raw.githubusercontent.com/ndmitchell/hlint/master/misc/appveyor.ps1'
 Invoke-Command ([Scriptblock]::Create($Script.Content)) -ArgumentList $HLINT_ARGUMENTS
 
-Set-Variable PATH "$PATH;$PWD" # Make sure stack.exe is on PATH, even if we change directory
-Set-Variable STACK_ROOT 'c:\\sr'
+$env:PATH += ";$PWD" # Make sure stack.exe is on PATH, even if we change directory
+$env:STACK_ROOT = 'c:\\sr'
 Invoke-WebRequest 'http://www.stackage.org/stack/windows-i386' -OutFile 'stack.zip'
 7z x -y stack.zip stack.exe
 
