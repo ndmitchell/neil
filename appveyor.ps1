473feec2d492fed44ac9b2509932f9ba5c9cbe5e
Neil Mitchell <ndmitchell@gmail.com>
2018-03-19 21:24:05 +0000

    Update appveyor.ps1

--- appveyor.ps1
+++ appveyor.ps1
@@ -35,7 +35,10 @@ if ($LASTEXITCODE -ne 0) {
 cmd /c '.\stack setup 1>&2 2>&1 > nul'
 if ($LASTEXITCODE -ne 0) {exit 1}
 
-cmd /c 'echo | .\stack --no-terminal build --verbose 2>&1'
+cmd /c 'echo | .\stack --no-terminal build network -v3 2>&1'
+if ($LASTEXITCODE -ne 0) {exit 1}
+
+cmd /c 'echo | .\stack --no-terminal build --test --bench --ghc-options=-rtsopts 2>&1'
 if ($LASTEXITCODE -ne 0) {exit 1}
 
 $Script = Invoke-WebRequest 'https://raw.githubusercontent.com/ndmitchell/weeder/master/misc/appveyor.ps1'
