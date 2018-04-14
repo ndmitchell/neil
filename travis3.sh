df76810ddca1b3a3a3d2c21cdc6f3399ad33fd14
Neil Mitchell <ndmitchell@gmail.com>
2018-04-14 22:33:52 +0100

    More work on travis3

--- travis3.sh
+++ travis3.sh
@@ -4,6 +4,9 @@
 set -e # exit on errors
 set -x # echo each line
 
+ls /opt/cabal
+ls /opt/ghc/*.*
+echo $HOME/.cabal/bin:/opt/ghc/$(ls /opt/ghc/*.*)/bin:/opt/cabal/$(ls /opt/cabal)/bin:$PATH
 export "PATH=$HOME/.cabal/bin:/opt/ghc/$(ls /opt/ghc/*.*)/bin:/opt/cabal/$(ls /opt/cabal)/bin:$PATH"
 
 cabal update && cabal install
