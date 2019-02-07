4ad6001f99895701f5eaf01d2109bb9516b36ae2
Neil Mitchell <ndmitchell@gmail.com>
2019-02-07 22:35:04 +0000

    Switch to run.sh

--- travis.sh
+++ travis.sh
@@ -37,7 +37,7 @@ timer(){
 if [ "$HLINT_ARGUMENTS" = "" ]; then
     HLINT_ARGUMENTS=.
 fi
-curl -sSL https://raw.github.com/ndmitchell/hlint/master/misc/travis.sh | sh -s $HLINT_ARGUMENTS
+curl -sSL https://raw.github.com/ndmitchell/hlint/master/misc/run.sh | sh -s $HLINT_ARGUMENTS
 
 if [ "$GHCVER" = "7.4" ]; then GHCVER=7.4.2; fi
 if [ "$GHCVER" = "7.6" ]; then GHCVER=7.6.3; fi
