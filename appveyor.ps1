e6f9c7e2dc1ec5d4017ba84c1dd0c8b82d33ac92
Neil Mitchell <ndmitchell@gmail.com>
2019-06-30 22:43:25 +0100

    More debuggin

--- appveyor.ps1
+++ appveyor.ps1
@@ -50,8 +50,8 @@ Write-Output "Before writing"
 Add-Content "stack.yaml" '\nghc-options: {"$locals": "-ddump-to-file -ddump-hi"}\n'
 Write-Output "After writing"
 
-Get-Content stack.yaml | foreach {Write-Output $_}
-
+cmd /c 'type stack.yaml'
+Write-Output "Total output done"
 
 cmd /c '.\stack setup 1>&2 2>&1 > nul'
 if ($LASTEXITCODE -ne 0) {exit 1}
