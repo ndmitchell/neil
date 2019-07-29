0b50c7766cba282179be58f89205541180b40eae
Neil Mitchell <ndmitchell@gmail.com>
2019-07-29 19:41:02 +0100

    Enable testing with GHC 8.8

--- src/Cabal.hs
+++ src/Cabal.hs
@@ -21,7 +21,7 @@ ghcReleases = ["7.10.3","8.0.2","8.2.2","8.4.4","8.6.5"]
 
 -- | The next version of GHC that has not yet been released
 --   (might test it in Travis, but won't be in the tested-with)
-ghcNext = Nothing -- set this to "8.8.1" once rc1 is out
+ghcNext = Just "8.8.1" -- set this to "8.8.1" once rc1 is out
 
 ghcWarnings = words "-fwarn-unused-binds -fwarn-unused-imports -fwarn-orphans"
 
