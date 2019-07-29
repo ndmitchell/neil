cb6bf66c85a389abf54ba203a902d3b5e6bdcd59
Neil Mitchell <ndmitchell@gmail.com>
2019-07-29 20:02:53 +0100

    Fix extend to work with ghcNext

--- src/Cabal.hs
+++ src/Cabal.hs
@@ -78,7 +78,7 @@ checkTravis = do
         fail $ "Expect to see exactly one script but not, please add: " ++ script
 
     claimed <- (\xs -> sort $ "head" : maybeToList ghcNext ++ xs) <$> testedWith
-    let extend x = fromMaybe x $ lookup (x++".") [(dropWhileEnd (/= '.') v, v) | v <- ghcReleases]
+    let extend x = fromMaybe x $ lookup (x++".") [(dropWhileEnd (/= '.') v, v) | v <- ghcReleases ++ maybeToList ghcNext]
     -- Add a nub since you might write an entry twice, once with expected_failures
     let tested = nubSort [extend $ fst $ word1 num | Just (_, num) <- map (stripInfix "GHCVER=") src]
     when (claimed /= tested) $
