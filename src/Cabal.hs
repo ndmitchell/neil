6bc3ed992af7703c18cd1ac4226564c318048826
Neil Mitchell <ndmitchell@gmail.com>
2020-07-17 21:47:00 +0100

    Move to GHC 8.8.4

--- src/Cabal.hs
+++ src/Cabal.hs
@@ -17,7 +17,7 @@ import Arguments
 import Prelude
 
 -- | GHC releases I test with
-ghcReleases = ["8.0.2","8.2.2","8.4.4","8.6.5","8.8.3","8.10.1"]
+ghcReleases = ["8.0.2","8.2.2","8.4.4","8.6.5","8.8.4","8.10.1"]
 
 -- | The next version of GHC that has not yet been released
 --   (might test it in Travis, but won't be in the tested-with)
