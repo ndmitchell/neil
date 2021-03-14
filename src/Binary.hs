092da65c4eec4f5d99d92e745395bcff834b6b64
Neil Mitchell <ndmitchell@gmail.com>
2021-03-14 17:40:55 +0000

    Don't use cabal v1-sdist, because it has disappeared in v3.4

--- src/Binary.hs
+++ src/Binary.hs
@@ -28,34 +28,39 @@ run Binary{..} = Just $ withCurrentDirectory path $ withTempDir $ \tdir -> do
     let grab tag = concat [words $ drop (length tag + 1) x | x <- relines $ lines src, (tag ++ ":") `isPrefixOf` x]
     let dataDir = case grab "data-dir" of [] -> id; [x] -> (x </>)
     let files = grab "extra-doc-files" ++ map dataDir (grab "data-files")
-    system_ $ "cabal v1-sdist --output-directory=" ++ tdir
+    system_ $ "cabal v2-sdist --output-directory=" ++ tdir
     let vname = name ++ "-" ++ ver
     let zname = if isWindows then vname ++ "-x86_64-windows.zip"
                 else if isMac then vname ++ "-x86_64-osx.tar.gz"
                 else vname ++ "-x86_64-linux.tar.gz"
     tdir <- canonicalizePath tdir
+    tdirFiles <- getDirectoryContents tdir
+    let [tdirTarball] = filter (".tar.gz" `isSuffixOf`) tdirFiles
+    let tarballName = dropExtension $ dropExtension tdirTarball
     b <- withCurrentDirectory tdir $ do
-        system_ "cabal v2-build --only-dependencies"
-        system_ "cabal v2-configure --datadir=nul --disable-library-profiling"
-        system_ "cabal v2-build"
-        let out = "bin" </> vname
-        builtFiles <- listFilesRecursive "dist-newstyle/build"
-        case find (\x -> takeFileName x == name <.> exe) builtFiles of
-            Nothing -> pure False
-            Just built -> do
-                copy built (out </> name <.> exe)
-                forM_ files $ \file -> when ('*' `notElem` file) $
-                    copy file $ out </> file
-                withCurrentDirectory "bin" $
-                    if isWindows then
-                        system_ $ "7z a " ++ zname ++ " " ++ vname
-                    else
-                        system_ $ "tar -czvf " ++ zname ++ " " ++ vname
-                pure True
+        system_ $ "tar -xf " ++ tdirTarball
+        withCurrentDirectory tarballName $ do
+            system_ "cabal v2-build --only-dependencies"
+            system_ "cabal v2-configure --datadir=nul --disable-library-profiling"
+            system_ "cabal v2-build"
+            let out = "bin" </> vname
+            builtFiles <- listFilesRecursive "dist-newstyle/build"
+            case find (\x -> takeFileName x == name <.> exe) builtFiles of
+                Nothing -> pure False
+                Just built -> do
+                    copy built (out </> name <.> exe)
+                    forM_ files $ \file -> when ('*' `notElem` file) $
+                        copy file $ out </> file
+                    withCurrentDirectory "bin" $
+                        if isWindows then
+                            system_ $ "7z a " ++ zname ++ " " ++ vname
+                        else
+                            system_ $ "tar -czvf " ++ zname ++ " " ++ vname
+                    pure True
     if not b then
         putStrLn "Completed, but package does not have a similarly named binary"
      else do
         let res = "dist/bin" </> zname
-        copy (tdir </> "bin" </> zname) res
+        copy (tdir </> tarballName </> "bin" </> zname) res
         putStrLn $ "Completed, produced " ++ res
 run _ = Nothing
