88673cc6d66357fcf292ca71648750c43b34951f
Neil Mitchell <ndmitchell@gmail.com>
2021-03-14 18:01:01 +0000

    Use v2-sdist to create the release tarball

--- github.sh
+++ github.sh
@@ -63,7 +63,7 @@ retry git clone --depth=1 "https://github.com/ndmitchell/neil" .neil
 
 if [ "$MAKE_RELEASE" = "true" ]; then
     .neil/neil bin
-    cabal v1-sdist
+    cabal v2-sdist
 else
     timer .neil/neil test --install --cabal2
     # Make sure the output is on $PATH
