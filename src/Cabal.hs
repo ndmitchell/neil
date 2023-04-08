545b3c8671d8e069a145ecd8a69820bcc4089d5d
Neil Mitchell <ndmitchell@gmail.com>
2023-04-08 11:48:05 +0100

    Require GHC 9.6, only support down to 8.8

--- src/Cabal.hs
+++ src/Cabal.hs
@@ -17,7 +17,7 @@ import Arguments
 import Prelude
 
 -- | GHC releases I test with
-ghcReleases = ["8.0","8.2","8.4","8.6","8.8","8.10","9.0","9.2","9.4"]
+ghcReleases = ["8.8","8.10","9.0","9.2","9.4", "9.6"]
 
 ghcWarnings = words "-Wunused-binds -Wunused-imports -Worphans"
 
